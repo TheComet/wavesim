5c97292c297e9009d2a1d2d8456f28499d257d54
TheComet <alex.murray@gmx.ch>
2018-11-07 02:42:07 +0100

    Reference counted init/deinit

--- wavesim/common/src/wavesim.c
+++ wavesim/common/src/wavesim.c
@@ -2,45 +2,33 @@
 #include "wavesim/memory.h"
 #include "wavesim/log.h"
 
-static int g_was_initialised = 0;
+static int g_init_counter = 0;
 
 /* ------------------------------------------------------------------------- */
 wsret
 wavesim_init(void)
 {
-    if (g_was_initialised)
-    {
-        ws_log_info(&g_ws_log, "WaveSim is already initialized, skipping...");
+    if (g_init_counter++)
         return WS_OK;
-    }
 
-    if (memory_init())
+    if (memory_init() != 0)
         return WS_ERR_OUT_OF_MEMORY;
 
     log_construct(&g_ws_log);
     ws_log_info(&g_ws_log, "Initializing WaveSim...");
 
-    g_was_initialised = 1;
-    return 0;
+    return WS_OK;
 }
 
 /* ------------------------------------------------------------------------- */
 int
 wavesim_deinit(void)
 {
-    if (!g_was_initialised)
+    if (--g_init_counter > 0)
         return 0;
 
     ws_log_info(&g_ws_log, "De-Initializing WaveSim...");
     log_destruct(&g_ws_log);
 
-    g_was_initialised = 0;
     return memory_deinit();
-}
-
-/* ------------------------------------------------------------------------- */
-int
-wavesim_is_initialized(void)
-{
-    return g_was_initialised;
 }
